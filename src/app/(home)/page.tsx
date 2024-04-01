4e342c4d48d378563492efa3b3e8664f506cc9d4
Lucas Trecente <lucastrecente@hotmail.com>
2024-04-01 16:39:36 -0300

    feat(home): create home page component with basic content and metadata

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl">Kyaria</h1>
+    </>
+  );
+}
