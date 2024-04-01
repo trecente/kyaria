00cc0acef1fd7ffbd6162567ef9c25c0d4c81316
Lucas Trecente <lucastrecente@hotmail.com>
2024-04-01 16:41:18 -0300

    feat(layout): implement root layout component

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+import "@/styles/globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Kyaria",
+    absolute: "Kyaria",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
