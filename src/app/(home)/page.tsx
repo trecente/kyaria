a3e1af2d458e71062225ce7816f36cab25797ffb
Lucas Trecente <lucastrecente@hotmail.com>
2024-04-03 01:45:14 -0300

    feat(app): update home page with hero section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,8 +6,16 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-3xl">Kyaria</h1>
-    </>
+    <main>
+      <div className="mx-auto max-w-sm space-y-2 py-10 text-center sm:max-w-xl sm:space-y-2 sm:py-14 md:max-w-3xl md:space-y-4 md:py-20">
+        <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl">
+          Find your <span className="text-blue-600">new job</span> today.
+        </h1>
+        <p className="text-base text-muted-foreground md:text-xl">
+          Thousands of jobs in the computer, engineering and technology sectors
+          are waiting for you.
+        </p>
+      </div>
+    </main>
   );
 }
