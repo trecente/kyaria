612ee95b70e496ceec1653ca0b08e2581879a7ff
Lucas Trecente <lucastrecente@hotmail.com>
2024-04-04 19:28:37 -0300

    feat(lib): add formatCurrency and formatDateToRelativeString functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,27 @@
-import { type ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+
+import { formatDistanceToNowStrict } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function formatCurrency(
+  value: number,
+  currency: string = "USD",
+  locale: string = "en-US",
+): string {
+  const options: Intl.NumberFormatOptions = {
+    currency,
+    style: "currency",
+    minimumFractionDigits: 2,
+  };
+  return new Intl.NumberFormat(locale, options).format(value);
+}
+
+export function formatDateToRelativeString(date: Date): string {
+  return formatDistanceToNowStrict(date, {
+    addSuffix: true,
+  });
 }
