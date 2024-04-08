65f1f64e5208ad6f4c761f85475d355b0e3298e1
Lucas Trecente <lucastrecente@hotmail.com>
2024-04-08 06:35:04 -0300

    feat(components): add conditional rendering for empty job list

--- src/app/(home)/_components/job-list.tsx
+++ src/app/(home)/_components/job-list.tsx
@@ -15,11 +15,17 @@ export async function JobList({ searchParams }: { searchParams: FilterType }) {
         </h1>
       </div>
 
-      <div className="space-y-2">
-        {jobs.map((job) => (
-          <JobCard key={job.id} job={job} />
-        ))}
-      </div>
+      {jobs.length > 0 ? (
+        <div className="space-y-2">
+          {jobs.map((job) => (
+            <JobCard key={job.id} job={job} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex justify-center">
+          <h1 className="text-xl font-semibold">Jobs not found.</h1>
+        </div>
+      )}
     </div>
   );
 }
