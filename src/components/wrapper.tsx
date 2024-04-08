742b917d8990daf33181db812bf87344dbe0cb5f
Lucas Trecente <lucastrecente@hotmail.com>
2024-04-08 08:26:52 -0300

    style(components): update wrapper to fill width

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -8,7 +8,9 @@ export function Wrapper({
   children: React.ReactNode;
 }) {
   return (
-    <div className={cn("mx-auto max-w-[1440px] px-4 sm:px-6", className)}>
+    <div
+      className={cn("mx-auto w-full max-w-[1440px] px-4 sm:px-6", className)}
+    >
       {children}
     </div>
   );
