fca3fe3ec70b18589e1a2150be4a82d3c869345c
Lucas Trecente <lucastrecente@hotmail.com>
2024-04-11 16:06:25 -0300

    refactor(lib): remove 'any' option from work_locations and employment_types

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,6 @@
-export const WORK_LOCATIONS = ["Any", "Remote", "Hybrid", "On-site"];
+export const WORK_LOCATIONS = ["Remote", "Hybrid", "On-site"];
 
 export const EMPLOYMENT_TYPES = [
-  "Any",
   "Full-Time",
   "Part-Time",
   "Contract",
