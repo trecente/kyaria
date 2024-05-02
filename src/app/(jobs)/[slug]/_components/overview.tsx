f3ba08de19146c5ef70a020c5f03c0d5b365c872
Lucas Trecente <lucastrecente@hotmail.com>
2024-05-02 13:04:40 -0300

    feat(components): add overview component to display job details

---
+++ src/app/(jobs)/[slug]/_components/overview.tsx
@@ -0,0 +1,81 @@
+import { formatDate } from "date-fns";
+import {
+  BriefcaseBusiness,
+  Building,
+  Calendar,
+  Clock,
+  Layers,
+  LucideIcon,
+  MapPin,
+} from "lucide-react";
+
+import { formatDateToRelativeString } from "@/lib/utils";
+
+interface OverviewProps {
+  details: {
+    companyName: string;
+    education: string | null;
+    jobLevel: string | null;
+    locationType: string;
+    createdAt: Date;
+    updatedAt: Date;
+  };
+}
+
+interface OverviewDataProps {
+  title: string;
+  icon: LucideIcon;
+  value: string;
+}
+
+export function Overview({
+  details: {
+    companyName,
+    education,
+    jobLevel,
+    locationType,
+    createdAt,
+    updatedAt,
+  },
+}: OverviewProps) {
+  const overviewData: OverviewDataProps[] = [
+    {
+      title: "Company",
+      icon: Building,
+      value: companyName,
+    },
+    {
+      title: "Level",
+      icon: Layers,
+      value: jobLevel || "Not specified",
+    },
+    {
+      title: "Education",
+      icon: BriefcaseBusiness,
+      value: education || "Not specified",
+    },
+    {
+      title: "Work Location",
+      icon: MapPin,
+      value: locationType,
+    },
+    {
+      title: "Last Updated",
+      icon: Clock,
+      value: formatDateToRelativeString(updatedAt),
+    },
+    {
+      title: "Posted",
+      icon: Calendar,
+      value: formatDate(createdAt, "dd MMM, yyyy"),
+    },
+  ];
+
+  return overviewData.map(({ title, icon: Icon, value }) => (
+    <div key={title} className="flex flex-col items-center overflow-hidden">
+      <Icon strokeWidth={1.5} className="text-blue-600 dark:text-blue-500" />
+      <small className="mt-2 font-medium text-muted-foreground">{title}</small>
+      <p className="text-sm font-semibold">{value}</p>
+    </div>
+  ));
+}
