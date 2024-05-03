23743603aee3b098adc353d305fb06c0e291fb6b
Lucas Trecente <lucastrecente@hotmail.com>
2024-05-03 16:25:20 -0300

    perf(lib): add caching to getJobs function

--- src/lib/fetchers.ts
+++ src/lib/fetchers.ts
@@ -6,45 +6,47 @@ import { cache } from "react";
 import { prisma } from "./prisma";
 import { FilterType } from "./schemas";
 
-export async function getJobs(searchParams: FilterType): Promise<Job[]> {
-  const { q, location, work: locationType, employment: type } = searchParams;
+export const getJobs = cache(
+  async (searchParams: FilterType): Promise<Job[]> => {
+    const { q, location, work: locationType, employment: type } = searchParams;
 
-  const searchQuery = q?.trim().split(/\s+/).filter(Boolean).join(" & ");
+    const searchQuery = q?.trim().split(/\s+/).filter(Boolean).join(" & ");
 
-  const searchFilter: Prisma.JobWhereInput = searchQuery
-    ? {
-        OR: [
-          { title: { search: searchQuery } },
-          { companyName: { search: searchQuery } },
-          { location: { search: searchQuery } },
-        ],
-      }
-    : {};
+    const searchFilter: Prisma.JobWhereInput = searchQuery
+      ? {
+          OR: [
+            { title: { search: searchQuery } },
+            { companyName: { search: searchQuery } },
+            { location: { search: searchQuery } },
+          ],
+        }
+      : {};
 
-  const filter: Prisma.JobWhereInput = {
-    AND: [
-      searchFilter,
-      location && { location: { equals: location } },
-      locationType && { locationType: { equals: locationType } },
-      type && { type: { equals: type } },
-      { approved: true },
-    ].filter(Boolean) as Prisma.JobWhereInput["AND"],
-  };
+    const filter: Prisma.JobWhereInput = {
+      AND: [
+        searchFilter,
+        location && { location: { equals: location } },
+        locationType && { locationType: { equals: locationType } },
+        type && { type: { equals: type } },
+        { approved: true },
+      ].filter(Boolean) as Prisma.JobWhereInput["AND"],
+    };
 
-  try {
-    const jobs = await prisma.job.findMany({
-      where: filter,
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
+    try {
+      const jobs = await prisma.job.findMany({
+        where: filter,
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
 
-    return jobs;
-  } catch (error) {
-    console.error(`Failed to fetch jobs: ${error}`);
-    return [];
-  }
-}
+      return jobs;
+    } catch (error) {
+      console.error(`Failed to fetch jobs: ${error}`);
+      return [];
+    }
+  },
+);
 
 export async function getDistinctLocations(): Promise<string[]> {
   try {
