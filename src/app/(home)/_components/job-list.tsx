fd663dfe287862322eb4b7bdc37c81557c4be2e7
Lucas Trecente <lucastrecente@hotmail.com>
2024-05-03 11:16:10 -0300

    feat(components): wrap job card in link

--- src/app/(home)/_components/job-list.tsx
+++ src/app/(home)/_components/job-list.tsx
@@ -1,31 +1,24 @@
-import { JobCard } from "./job-card";
+import Link from "next/link";
 
 import { getJobs } from "@/lib/fetchers";
 import { FilterType } from "@/lib/schemas";
+
+import { JobCard } from "./job-card";
 
 export async function JobList({ searchParams }: { searchParams: FilterType }) {
   const jobs = await getJobs(searchParams);
 
   return (
-    <div className="xl:space-y-4">
-      <div className="flex justify-center md:block">
-        <h1 className="py-4 text-lg font-semibold tracking-tight xl:py-0 xl:text-base">
-          Showing <span className="text-blue-600">{jobs.length}</span> available
-          jobs
-        </h1>
-      </div>
-
-      {jobs.length > 0 ? (
-        <div className="space-y-2">
-          {jobs.map((job) => (
-            <JobCard key={job.id} job={job} />
-          ))}
-        </div>
-      ) : (
-        <div className="flex justify-center">
-          <h1 className="text-xl font-semibold">Jobs not found.</h1>
-        </div>
-      )}
+    <div className="space-y-2">
+      {jobs.map((job) => (
+        <Link
+          key={job.id}
+          href={job.slug}
+          className="block transition-all duration-100 hover:scale-[1.01]"
+        >
+          <JobCard job={job} />
+        </Link>
+      ))}
     </div>
   );
 }
