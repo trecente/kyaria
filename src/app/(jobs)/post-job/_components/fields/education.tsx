5a7a140cb63f83a1bd600e74e801b876877438b0
Lucas Trecente <lucastrecente@hotmail.com>
2024-05-04 17:53:45 -0300

    feat(components): add form fields for education and job level

---
+++ src/app/(jobs)/post-job/_components/fields/education.tsx
@@ -0,0 +1,59 @@
+import { Control } from "react-hook-form";
+
+import { EDUCATION_TYPE_OPTIONS } from "@/lib/constants";
+import { CreateJobType } from "@/lib/schemas";
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+interface EducationFieldProps {
+  control: Control<CreateJobType>;
+  isSubmitting: boolean;
+}
+
+export function EducationField({ control, isSubmitting }: EducationFieldProps) {
+  return (
+    <FormField
+      control={control}
+      name="education"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Education</FormLabel>
+
+          <Select
+            onValueChange={field.onChange}
+            defaultValue={field.value}
+            disabled={isSubmitting}
+          >
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder="Select an education" />
+              </SelectTrigger>
+            </FormControl>
+
+            <SelectContent>
+              {EDUCATION_TYPE_OPTIONS.map((type) => (
+                <SelectItem key={type} value={type}>
+                  {type}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
---
+++ src/app/(jobs)/post-job/_components/fields/level.tsx
@@ -0,0 +1,59 @@
+import { Control } from "react-hook-form";
+
+import { LEVEL_OPTIONS } from "@/lib/constants";
+import { CreateJobType } from "@/lib/schemas";
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+interface LevelFieldProps {
+  control: Control<CreateJobType>;
+  isSubmitting: boolean;
+}
+
+export function LevelField({ control, isSubmitting }: LevelFieldProps) {
+  return (
+    <FormField
+      control={control}
+      name="jobLevel"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Level</FormLabel>
+
+          <Select
+            onValueChange={field.onChange}
+            defaultValue={field.value}
+            disabled={isSubmitting}
+          >
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder="Select a level" />
+              </SelectTrigger>
+            </FormControl>
+
+            <SelectContent>
+              {LEVEL_OPTIONS.map((type) => (
+                <SelectItem key={type} value={type}>
+                  {type}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
