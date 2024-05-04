cf9aafeaca510f4b6ed634f4876957d30819eefc
Lucas Trecente <lucastrecente@hotmail.com>
2024-05-04 17:46:30 -0300

    feat(lib): add new constant arrays for education and job levels and make all constants read-only

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,27 @@
-export const WORK_LOCATIONS = ["Remote", "Hybrid", "On-site"];
+export const WORK_LOCATIONS: Readonly<string[]> = [
+  "Remote",
+  "Hybrid",
+  "On-site",
+];
 
-export const EMPLOYMENT_TYPES = [
+export const EMPLOYMENT_TYPES: Readonly<string[]> = [
   "Full-Time",
   "Part-Time",
   "Contract",
   "Temporary",
   "Internship",
+];
+
+export const EDUCATION_TYPE_OPTIONS: Readonly<string[]> = [
+  "High school",
+  "Associate's degree",
+  "Bachelor's degree",
+  "Master's degree",
+  "Doctoral degree (PhD)",
+];
+
+export const LEVEL_OPTIONS: Readonly<string[]> = [
+  "Entry-level",
+  "Mid-level",
+  "Senior-level",
 ];
