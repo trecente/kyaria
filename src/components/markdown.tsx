970e2628ba1750dca4d93030ce71c678852007ff
Lucas Trecente <lucastrecente@hotmail.com>
2024-05-15 12:05:31 -0300

    feat(components): enable GFM support and raw HTML rendering in markdown

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,4 +1,7 @@
 import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import rehypeSanitize from "rehype-sanitize";
+import remarkGfm from "remark-gfm";
 
 import { cn } from "@/lib/utils";
 
@@ -9,7 +12,11 @@ interface MarkdownProps {
 
 export function Markdown({ children, className }: MarkdownProps) {
   return (
-    <ReactMarkdown className={cn("min-w-full break-words", className)}>
+    <ReactMarkdown
+      className={cn("min-w-full break-words", className)}
+      rehypePlugins={[rehypeRaw, rehypeSanitize]}
+      remarkPlugins={[remarkGfm]}
+    >
       {children}
     </ReactMarkdown>
   );
